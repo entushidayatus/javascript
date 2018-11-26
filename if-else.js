6b7f27dd50628aba94a426fa7828f9002c396126
hidayatus solihin <entus.hidayat@gmail.com>
2018-11-26 13:24:45 +0700

    git commit "push pertama" -m

---
+++ if-else.js
@@ -0,0 +1,29 @@
+function jalanSatu() {
+    var nilai = 3;
+
+    if (nilai === 2) {
+        console.log('Benar')
+    } else if (nilai === 3) {
+        console.log('Tiga')
+    } else {
+        console.log('Salah')
+    }
+}
+
+jalanSatu()
+
+function jalanDua(){
+    var makan = 0
+    
+    if (makan == 7) {
+        console.log('breakfast')
+    } else if (makan == 12) {
+        console.log('lunch')
+    } else if (makan == 18) {
+        console.log('dinner')
+    } else {
+        console.log('belum makan')
+    }
+}
+
+jalanDua()
